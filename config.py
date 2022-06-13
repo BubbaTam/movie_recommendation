08f5d31dc5f693cd26f993abe5e875cb9dd55dfa
BubbaTam <jamesglassey@outlook.com>
2022-06-13 13:41:16 +0100

    feat: add config file from previous repo

---
+++ config.py
@@ -0,0 +1,5 @@
+RAW_DATA_CREDITS = r'data_sources\raw\tmdb_5000_credits.csv'
+RAW_DATA_MOVIES = r'data_sources\raw\tmdb_5000_movies.csv'
+PROCESSED_DATAFOLDER = r'data_sources\processed'
+DATA_1 = r"data_sources\processed\basic_data_1.csv"
+SAVED_PARAMETER_FOLDER = r"saved_entities\parameters"
